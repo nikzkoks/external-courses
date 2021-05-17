45657d14c52cd38f0d3a26b0ad33dbabc3b5b99b
Nikita Khobotov <n1kz.k0ks@yandex.ru>
2021-05-17 21:48:54 +0300

    Fix task02 :)

--- src/ex16_js-async/task-02.js
+++ src/ex16_js-async/task-02.js
@@ -1,31 +1,45 @@
-search = document.getElementById('search');
+const search = document.getElementById('search');
+const text = document.getElementById('text');
 
-const time = 1000;
-const mockArray = ['red', 'green', 'blue'];
+const time = 200;
+const mockArray = [
+	'123',
+	'234',
+	'312',
+	'red',
+	'rad',
+	'black',
+	'blur',
+	'green',
+	'glass',
+];
 
 console.log(mockArray);
 
-function searchItem() {
-  for (let key of mockArray) {
-    let value = search.value;
-    if (key[0] === value[0]) {
-      console.log(key);
-      console.log(value);
-    }
-  }
+function searchItem(...rest) {
+	let searchValue = search.value;
+	mockArray.forEach((item) => {
+		if (item.includes(searchValue) && item[0] === searchValue[0]) {
+			text.value = item;
+		}
+	});
+	if (!searchValue) {
+		text.value = null;
+	}
+	console.log(rest);
 }
 
 function debounce(callback) {
-  let isRunning = false;
+	let isRunning = false;
 
-  return function () {
-    if (isRunning) return;
-    callback.apply(this, arguments);
-    isRunning = true;
-    setTimeout(() => {
-      isRunning = false;
-    }, time);
-  };
+	return function () {
+		if (isRunning) return;
+		callback.apply(this, ...rest);
+		isRunning = true;
+		setTimeout(() => {
+			isRunning = false;
+		}, time);
+	};
 }
 
-search.addEventListener('input', debounce(searchItem));
+search.addEventListener('input', debounce(searchItem(1, 2, 3)));
